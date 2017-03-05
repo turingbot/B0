05f76b95669300ba606ee560f7729c04ce3ee0e6
mohsen67t <mohsen.tb4@gmail.com>
2017-03-05 14:53:05 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,10 @@ function create_config( )
 		"fun",
 		"info"
 	},
-    sudo_users = {157059515},
+    sudo_users = {
+    151499613,
+    YourID
+  },
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
