babbc43792b26765919d3856ba10eaefef513a42
turingbot <tabaghi.m@gmail.com>
2017-04-02 00:52:56 +0430

    Update mute-time.lua

--- plugins/mute-time.lua
+++ plugins/mute-time.lua
@@ -1,42 +1,141 @@
-
+do
 local function pre_process(msg)
-  local hash = 'mute_time:'..msg.chat_id_
-  if redis:get(hash) and gp_type(msg.chat_id_) == 'channel' and not is_admin(msg)  then
-    tdcli.deleteMessages(msg.chat_id_, {[0] = tonumber(msg.id_)})
-  end
+ local hash = 'muteall:'..msg.to.id
+  if redis:get(hash) and msg.to.type == 'channel' and not is_mod(msg)  then
+    tdcli.deleteMessages(msg.chat_id_, {[0] = msg.id_})
+       end
+    return msg
  end
  
 local function run(msg, matches)
-  if matches[1]:lower() == 'mt' and is_admin(msg) or matches[1]:lower() == 'تعطیل' and is_admin(msg) then
-     local hash = 'mute_time:'..msg.chat_id_
-     if not matches[2] then
-		return "_لطفا ساعت و دقیقه را وارد نمایید!_"
-  else
-     local hour = string.gsub(matches[2], 'h', '')
-     local num1 = tonumber(hour) * 3600
-     local minutes = string.gsub(matches[3], 'm', '')
-     local num2 = tonumber(minutes) * 60
-     local num4 = tonumber(num1 + num2)
-	 redis:setex(hash, num4, true)
-     return "⛔️گروه به مدت: \n`"..matches[2].."` ساعت\n`"..matches[3].."` دقیقه \nتعطیل میباشد.️"
-    end
+local hash = "gp_lang:"..msg.to.id
+local lang = redis:get(hash)
+ if matches[1] == 'mute' and matches[2] == 'all' and is_mod(msg) then
+       local hash = 'muteall:'..msg.to.id
+       if not matches[3] then
+              redis:set(hash, true)
+             return "mute all has been enabled"
+ else
+local hour = string.gsub(matches[3], 'h', '')
+local num1 = tonumber(hour) * 3600
+local minutes = string.gsub(matches[4], 'm', '')
+local num2 = tonumber(minutes) * 60
+local second = string.gsub(matches[5], 's', '')
+local num3 = tonumber(second) 
+local num4 = tonumber(num1 + num2 + num3)
+redis:setex(hash, num4, true)
+if not lang then
+ return "Mute all has been enabled for \n⏺ hours : "..matches[3].."\n⏺ minutes : "..matches[4].."\n⏺ seconds : "..matches[5].."\n@TuriingBot"
+ elseif lang then
+ return "بی صدا کردن فعال شد در \n⏺ ساعت : "..matches[3].."\n⏺ دقیقه : "..matches[4].."\n⏺ ثانیه : "..matches[5].."\n@TuriingBot"
+ end
+ end
+ end
+ if matches[1] == 'mute' and matches[2] == 'hours' and is_mod(msg) then
+       local hash = 'muteall:'..msg.to.id
+       if not matches[3] then
+              redis:set(hash, true)
+             return "mute all has been enabled"
+ else
+local hour = string.gsub(matches[3], 'h', '')
+local num1 = tonumber(hour) * 3600
+local num4 = tonumber(num1)
+redis:setex(hash, num4, true)
+if not lang then
+ return "Mute all has been enabled for \n⏺ hours : "..matches[3].."\n@TuriingBot"
+ elseif lang then
+ return "بی صدا کردن فعال شد در \n⏺ ساعت : "..matches[3].."\n@TuriingBot"
+ end
+ end
+ end
+  if matches[1] == 'mute' and matches[2] == 'minutes' and is_mod(msg) then
+       local hash = 'muteall:'..msg.to.id
+       if not matches[3] then
+              redis:set(hash, true)
+             return "mute all has been enabled"
+ else
+local minutes = string.gsub(matches[3], 'm', '')
+local num2 = tonumber(minutes) * 60
+local num4 = tonumber(num2)
+redis:setex(hash, num4, true)
+if not lang then
+ return "Mute all has been enabled for \n⏺ minutes : "..matches[3].."\n@TuriingBot"
+ elseif lang then
+ return "بی صدا کردن فعال شد در \n⏺ دقیقه : "..matches[3].."\n@TuriingBot"
+ end
+ end
+ end
+  if matches[1] == 'mute' and matches[2] == 'seconds' and is_mod(msg) then
+       local hash = 'muteall:'..msg.to.id
+       if not matches[3] then
+              redis:set(hash, true)
+             return "mute all has been enabled"
+ else
+local second = string.gsub(matches[3], 's', '')
+local num3 = tonumber(second) 
+local num4 = tonumber(num3)
+redis:setex(hash, num3, true)
+if not lang then
+ return "Mute all has been enabled for \n⏺ seconds : "..matches[3].."\n@TuriingBot"
+ elseif lang then
+ return "بی صدا کردن فعال شد در \n⏺ ثانیه : "..matches[3].."\n@TuriingBot"
+ end
+ end
+ end
+if matches[1] == 'helpmute' then
+if not lang then
+text = [[
+*Beyond Mute Commands:*
+*!mute all* `(hour) (minute) (seconds)`
+_Mute group at this time_ 
+*!mute hours* `(number)`
+_Mute group at this time_ 
+*!mute minute* `(number)`
+_Mute group at this time_ 
+*!mute seconds* `(number)`
+_Mute group at this time_ 
+*!unmute all*
+_Unmute group at this time_ 
+_You can use_ *[!/#]* _at the beginning of commands._				
+]]
+elseif lang then
+text = [[
+*راهنمای بیصدا های ربات بیوند:*
+*!mute all* `(hour) (minute) (seconds)`
+_بیصدا کردن گروه با ساعت و دقیقه و ثانیه_ 
+*!mute hours* `(number)`
+_بیصدا کردن گروه در ساعت_ 
+*!mute minute* `(number)`
+_بیصدا کردن گروه در دقیقه_ 
+*!mute seconds* `(number)`
+_بیصدا کردن گروه در ثانیه_ 
+*!unmute all*
+_آزاد سازی بیصدایی گروه در آن زمان_ 
+*شما میتوانید از [!/#] در اول دستورات برای اجرای آنها بهره بگیرید*
+]]
+end
+return text
+end
+if matches[1] == 'unmute' and matches[2] == 'all' and is_mod(msg) then
+               local hash = 'muteall:'..msg.to.id
+        redis:del(hash)
+		if not lang then
+          return "mute all has been disabled"
+		  elseif lang then
+		  return "گروه ازاد شد و افراد می توانند دوباره پست بگذارند"
   end
-  if matches[1]:lower() == 'unmt' and is_admin(msg) or matches[1]:lower() == 'لغو تعطیل' and is_admin(msg) then
-     local hash = 'mute_time:'..msg.chat_id_
-     redis:del(hash)
-     return "*✅گروه برای ارسال پیام کاربران باز شد.*"
-  end
+end
 end
 return {
    patterns = {
-      '^[/!#]([Mm][Tt])$',
-	  '^(تعطیل)$',
-      '^[/!#]([Uu][Nn][Mm][Tt])$',
-	  '^(لغو تعطیل)$',
-	  '^[/!#]([Mm][Tt]) (%d+) (%d+)$',
-	  '^(تعطیل) (%d+) (%d+)$',
+'([Uu]nmute) (all)$',
+'([Hh]elpmute)$',
+'([Mm]ute) (all) (.*) (.*) (.*)$',
+'([Mm]ute) (hours) (.*)$',
+'([Mm]ute) (minutes) (.*)$',
+'([Mm]ute) (seconds) (.*)$',
  },
-  run = run,
-  pre_process = pre_process
+run = run,
+pre_process = pre_process
 }
-
+end
